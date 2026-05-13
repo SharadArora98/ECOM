92343ac2b4d3855afb45bf7c50e37a6becb5cf48
schmoop <sarora@arkaenergy.com>
2026-05-13 22:04:07 +0530

    added the redis rate limitter in login api

---
+++ middleware/rateLimitMiddleware.js
@@ -0,0 +1,37 @@
+import { rateLimit } from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import Redis from "ioredis";
+
+const redisClient = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+redisClient.on("error", (err) => {
+  console.error("Redis Connection Error:", err);
+});
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, 
+  message: {
+    status: 429,
+    message: "Too many login attempts, please try again after 15 minutes",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+// export const apiLimiter = rateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   limit: 100,
+//   message: {
+//     status: 429,
+//     message: "Too many requests from this IP, please try again later.",
+//   },
+//   standardHeaders: true,
+//   legacyHeaders: false,
+//   store: new RedisStore({
+//     sendCommand: (...args) => redisClient.call(...args),
+//   }),
+// });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import authController from "../controller/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import { loginLimiter } from "../middleware/rateLimitMiddleware.js";
 
 const router = express.Router();
 
 // Register route
 router.post("/register", authController.register);
 // Login route
-router.post("/login", authController.login);
+router.post("/login", loginLimiter, authController.login);
 router.post("/changePassword", authMiddleware, authController.changePassword);
 
 export default router;
